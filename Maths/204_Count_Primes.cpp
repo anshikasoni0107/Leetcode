a343fd583e53ff2c8910f5cfb352ebe5f0f82d6f
anshikasoni0107 <sonianshika0107@gmail.com>
2026-04-12 19:09:48 +0530

    Create 204_Count_Primes.cpp

---
+++ Maths/204_Count_Primes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2)
+            return 0;
+            
+        vector<bool> p(n, true);
+        
+        p[0]=p[1]=false;
+ 
+        for (int i=2; i*i<n; i++)
+        {
+            if(p[i])
+            {
+                for(int j=i*i; j<n; j+=i)
+                {
+                    p[j]=false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=2; i<n; i++)
+        {
+            if(p[i]) count++;
+        }
+        return count;
+    }
+};
